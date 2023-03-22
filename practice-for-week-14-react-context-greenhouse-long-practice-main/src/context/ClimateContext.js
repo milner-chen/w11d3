6081f4cb346d9f9c0add951c355d0026ac91a205
Zane <zeisen33@gmail.com>
2023-03-22 14:00:37 -0400

    Phase 5

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -11,9 +11,10 @@ export const useClimate = () => useContext(ClimateContext);
 export default function ClimateProvider({ children }) {
 
     const [temperature, setTemperature] = useState(50);
+    const [humidity, setHumidity] = useState(40);
 
     return (
-        <ClimateContext.Provider value={{ temperature, setTemperature }}>
+        <ClimateContext.Provider value={{ temperature, setTemperature, humidity, setHumidity }}>
             {children}
         </ClimateContext.Provider>
     )
