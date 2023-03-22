79748ed7a2e3fb27258ac21b850f5da2b93300de
Milner Chen <milnerchen0@gmail.com>
2023-03-22 12:47:19 -0400

    finish phase 3

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,20 @@
+import { useContext, useState, createContext } from "react";
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext);
+// remember to set custom hook as a function
+
+export default function ClimateProvider({ children }) {
+
+    const [temperature, setTemperature] = useState(50);
+
+    return (
+        <ClimateContext.Provider value={{ temperature, setTemperature }}>
+            {children}
+        </ClimateContext.Provider>
+    )
+}
--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/index.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ThemeProvider from './context/ThemeContext';
+import ClimateProvider from './context/ClimateContext';
 
 function Root() {
   return (
+    <BrowserRouter>
       <ThemeProvider>
-        <BrowserRouter>
+        <ClimateProvider>
           <App />
-        </BrowserRouter>
+        </ClimateProvider>
       </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
