ab8a76cf440c55b21d1b645fc4b35d93bb6877c4
Milner Chen <milnerchen0@gmail.com>
2023-03-22 11:53:50 -0400

    finish phase 1

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,22 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
 
+  const { themeName, setThemeName } = useTheme();
+  const img = () => themeName === "day" ? dayImage : nightImage;
+  console.log(img());
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={img()}
             alt='greenhouse' 
       />
       <LightSwitch />
